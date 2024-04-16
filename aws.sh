1c48feebc5b700173cdfb93b325cafddb8be581d
Krish <98233937+krishpy3@users.noreply.github.com>
2024-04-16 17:39:29 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,25 +1,15 @@
-#/bin/bash
-instance=$(cat file.json)
+#!/bin/bash
 
-while IFS= read -r line; do
-    if [[ $line =~ \"Id\":\ \"(i-.+?)\" ]]; then
-        # echo "${line}"
-        # echo "${BASH_REMATCH[1]}"
-        instance_ids+=("${BASH_REMATCH[1]}")
-    fi
+# Define the table name and key schema
+TABLE_NAME="ExampleTable"
+HASH_KEY="Id"
+HASH_KEY_TYPE="S"  # 'S' stands for String, 'N' for Number
 
-done <<< "$instance"
+# Create DynamoDB table
+aws dynamodb create-table \
+    --table-name $TABLE_NAME \
+    --attribute-definitions AttributeName=$HASH_KEY,AttributeType=$HASH_KEY_TYPE \
+    --key-schema AttributeName=$HASH_KEY,KeyType=HASH \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
 
-# Iterate through each instance ID and get volume information
-for instance_id in "${instance_ids[@]}"; do
-    # Query volumes associated with the instance ID
-    instance_volumes=$(aws ec2 describe-volumes --filters Name=attachment.instance-id,Values="$instance_id" --query 'Volumes[*].{ID:VolumeId}' --output text)
-
-    # Iterate through each volume ID and get the volume information
-    while IFS= read -r volume_id; do
-        # Query volume information
-        volume_info=$(aws ec2 describe-volumes --volume-ids "$volume_id" --query 'Volumes[*].{ID:VolumeId,Size:Size,Type:VolumeType,State:State,Device:Attachments[0].Device}' --output text)
-
-        # Print volume information
-        echo "$volume_info"
-    done <<< "$instance_volumes"
+echo "DynamoDB table $TABLE_NAME created successfully."
